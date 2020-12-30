4064215f02d134bb78ce3d485556ede4cd3a653d
Janardhan Anipireddy <72906105+areddy548@users.noreply.github.com>
2020-12-30 10:31:05 +0530

    Update awx_install.sh

--- awx_install.sh
+++ awx_install.sh
@@ -3,16 +3,17 @@
 # Install packages required to setup AWX
 sudo yum -y install epel-release
 sudo yum -y update all
-sudo yum -y install -y wget git gettext ansible docker nodejs npm gcc-c++ bzip2 vim python-devel python2-devel pytest
+sudo yum -y install -y wget git gettext ansible docker nodejs npm gcc-c++ bzip2 vim python-devel python3-devel pytest
 
 
-sudo yum -y install python-setuptools
-sudo yum -y install python-pip
-pip install --upgrade pip
-pip install --upgrade setuptools
+sudo yum -y install python3-setuptools
+sudo yum -y install python3-pip
+sudo yum install libselinux-python3
+pip3 install --upgrade pip3
+pip3 install --upgrade setuptools
 #alias python=/usr/local/bin/python2.7
-pip install docker
-pip install docker-compose
+pip3 install docker
+pip3 install docker-compose
 
 # Creating the docker storage setup to ensure we have a docker thin pool 
 cat <<EOF > /etc/sysconfig/docker-storage-setup
@@ -34,4 +35,4 @@ systemctl enable docker
 git clone https://github.com/ansible/awx.git /tmp/awx_repo
 
 # Install AWX
-ansible-playbook -i /tmp/awx_repo/installer/inventory /tmp/awx_repo/installer/install.yml --extra-vars "dockerhub_version=latest"
+ansible-playbook -i /tmp/awx_repo/installer/inventory /tmp/awx_repo/installer/install.yml
